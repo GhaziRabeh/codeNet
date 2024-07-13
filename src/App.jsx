7632c4fb0e6680983726abb7a5cd10bce5386f9a
unknown <GHAZI@DESKTOP-NLN3HOV>
2024-07-13 19:31:50 +0100

    AI

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './comp/side comp/Sidebar'
+import Main from './comp/main/Main'
+
+const App = () => {
+  return (
+  <>
+  <Sidebar/>
+  <Main/>
+  </>
+  )
+}
+
+export default App
